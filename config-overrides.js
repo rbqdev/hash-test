384673ea10265a90185338f9cb9b7ed496d41c1d
Robson Braga <rbq.dev@gmail.com>
2021-03-06 11:26:53 -0300

    update(): update alias

--- config-overrides.js
+++ config-overrides.js
@@ -7,7 +7,9 @@ module.exports = function override(config) {
     alias: {
       ...config.alias,
       "@components": path.resolve(__dirname, "src/components"),
+      "@configs": path.resolve(__dirname, "src/configs"),
       "@contexts": path.resolve(__dirname, "src/contexts"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
       "@styles": path.resolve(__dirname, "src/styles"),
       "@utils": path.resolve(__dirname, "src/utils")
     }
