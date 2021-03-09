d6cb00042df4513234c0d122cecc012ee74d1b2f
Robson Braga <rbq.dev@gmail.com>
2021-03-09 11:59:20 -0300

    tests(unit): add toast tests

---
+++ src/components/Toast/Toast.test.tsx
@@ -0,0 +1,21 @@
+import render from "@testsUtils/renderWithTheme";
+import Toast, { ToastType } from ".";
+
+describe("Component: Toast", () => {
+  const mockMessage = "testMessage";
+
+  it("should render message", () => {
+    const { queryByText } = render(<Toast type="info" message={mockMessage} />);
+    expect(queryByText(mockMessage)).toBeInTheDocument();
+  });
+
+  test.each([
+    ["success" as ToastType],
+    ["error" as ToastType],
+    ["info" as ToastType],
+    ["warning" as ToastType]
+  ])("should render icon with type %s passed", type => {
+    const { getByTestId } = render(<Toast type={type} message={mockMessage} />);
+    expect(getByTestId(type)).toBeInTheDocument();
+  });
+});
