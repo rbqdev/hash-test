47db9fe2d565771292c844c597aca29eb80898b5
Robson Braga <rbq.dev@gmail.com>
2021-03-12 16:29:01 -0300

    storybook(Loader): add stories

---
+++ src/components/Loader/Loader.stories.tsx
@@ -0,0 +1,12 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+import Loader from ".";
+
+export default {
+  title: "Loader",
+  component: Loader,
+  args: {
+    customText: ""
+  }
+} as Meta;
+
+export const Default: Story = args => <Loader {...args} />;
