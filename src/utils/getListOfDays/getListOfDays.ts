fae8b6a73a10444a3822459932d4107793390d6e
Robson Braga <rbq.dev@gmail.com>
2021-03-13 20:29:46 -0300

    fix(getListOfDays): fix bug when length of custom day is 0

--- src/utils/getListOfDays/getListOfDays.ts
+++ src/utils/getListOfDays/getListOfDays.ts
@@ -6,8 +6,9 @@ export const DEFAULT_DAYS = [1, 15, 30, 90];
 export default function getListOfDays(): number[] {
   try {
     const urlParams = getParamsFromUrl();
-    const customDays = urlParams.get(KEY_PARAM_CUSTOM_DAYS)!;
-    return [...JSON.parse(customDays)].map(day => Number(day));
+    const customDaysParam = urlParams.get(KEY_PARAM_CUSTOM_DAYS)!;
+    const customDays = [...JSON.parse(customDaysParam)].map(day => Number(day));
+    return customDays.length > 0 ? customDays : DEFAULT_DAYS;
   } catch {
     return DEFAULT_DAYS;
   }
